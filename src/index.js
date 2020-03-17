0a09f3ccaa4791cd67de80216d8d64bf25042e10
stod_dan <dan@peripat.com>
2020-03-17 22:33:08 +0000

    init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-"use strict";
-
-let NewOneWithParameters = (a, b) => {
-  console.log(a + b); // 30
-};
-NewOneWithParameters(10, 20);
+//https://gist.github.com/ncochard/6cce17272a069fdb4ac92569d85508f4
+//https://github.com/facebook/jest/issues/2442
+export * from './config-ui';
+export * from './config-shared';
